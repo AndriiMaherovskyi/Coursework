d4af020546b1c49fabf72434e6cf911104cb10e1
Yura0Bodnar <yuriy575721@gmail.com>
2024-05-12 23:08:03 +0300

    created 1492 arrays

--- main.py
+++ main.py
@@ -38,6 +38,29 @@ def arr_for_map(grouped_with_country, grouped_with_country_s):
 
 
 
+# Функція для збору даних на теплову карту
+def arr_for_map(grouped_with_country, grouped_with_country_s):
+    for country_data in grouped_with_country:
+        # Отримуємо ключ країни
+        country_key = list(country_data.keys())[0]
+
+        # Шукаємо індекс елементу з таким ключем у grouped_with_country_s
+        index_country_s = next((i for i, item in enumerate(grouped_with_country_s) if country_key in item), None)
+
+        # Якщо знайдено індекс, додаємо дані з grouped_with_country_s до відповідного масиву
+        if index_country_s is not None:
+            # Отримуємо значення для ключа з grouped_with_country_s
+            country_s_values = grouped_with_country_s[index_country_s][country_key]
+
+            # Отримуємо значення для ключа з grouped_with_country
+            country_values = country_data[country_key]
+
+            # Злиття значень та оновлення значення в grouped_with_country
+            country_data[country_key] = np.concatenate((country_values, country_s_values))
+
+    return grouped_with_country
+
+
 def output_scores(X, labels):
     # Оцінюємо за допомогою різних метрик:
     silhouette_avg = silhouette_score(X, labels)
@@ -58,35 +81,33 @@ cluster_names = [
     "Задовільний"
 ]
 
+
+def kluster_all(last_column):
+
+    last_elements = df[last_column].to_numpy()  # перетворюємо у масив NumPy
+
+    # Заміна значень '..' на 0 та перетворення на float
+    last_elements = np.where(last_elements == '..', 0, last_elements)  # замінюємо '..' на 0
+    last_elements = last_elements.astype(float)  # перетворюємо в float
+
+    group_size = 217
+    num_groups = len(last_elements) // group_size
+    grouped_array = np.array(np.array_split(last_elements[:num_groups * group_size], num_groups))
+
+    return grouped_array
+
+
 if __name__ == "__main__":
 
-    # --- --- -- Two indicators --- --- ---
-    k = 4
-    df = pd.read_csv('Indicators-2.csv')
+    df = pd.read_csv('Worldbank-data-all.csv')
     first_column = df.columns[0]  # Перша колонка містить назву країни
     last_column = df.columns[-1]  # Остання колонка містить числові дані
-    grouped_with_country = make_indicator_dictionary(first_column, last_column, 'e')
-    print(f"Countries quantity(two indicators): {len(grouped_with_country)}")
+    grouped_with_indicators = kluster_all(last_column)
 
-    clusters_e_gpu, centroids_e_gpu = k_means(grouped_with_country, k, 'GPU')
-    show_clusters(clusters_e_gpu)
-    print()
-
-    # --- --- -- Capitals --- --- ---
-    k = 7
-    # Читаємо CSV-файл
-    capitals = pd.read_csv('capitals-location.csv')
-    name_column = capitals.columns[0]  # Перша колонка містить назву країни
-    coordinate_column = capitals.columns[2:4]  # Остання колонка містить числові дані
-
-    grouped_with_country_s = make_capital_dictionary(name_column, coordinate_column)
-    print("\n --- --- --- --- --- --- --- --- --- --- ---\n")
-    print(f"Countries quantity(capitals): {len(grouped_with_country_s)}")
-
-    clusters_f_gpu, centroids_f_gpu = k_means(grouped_with_country_s, k, 'GPU')
-
-    print("Capitals clusters (GPU):")
-    show_clusters(clusters_f_gpu)
+    start_time_s = time()
+    clusters_s, centroids_s = k_means(grouped_with_country, 7, 'CPU')
+    cpu_time = time() - start_time_s
+    show_clusters(clusters_s)
 
     # Вивід даних для теплової карти
 
@@ -107,6 +128,45 @@ if __name__ == "__main__":
 
 
 
+
+
+
+    # # --- --- -- Two indicators --- --- ---
+    # k = 4
+    # df = pd.read_csv('Indicators-2.csv')
+    # first_column = df.columns[0]  # Перша колонка містить назву країни
+    # last_column = df.columns[-1]  # Остання колонка містить числові дані
+    # grouped_with_country = make_indicator_dictionary(first_column, last_column, 'e')
+    # print(f"Countries quantity(two indicators): {len(grouped_with_country)}")
+#
+    # clusters_e_gpu, centroids_e_gpu = k_means(grouped_with_country, k, 'GPU')
+    # show_clusters(clusters_e_gpu)
+    # print()
+#
+    # # --- --- -- Capitals --- --- ---
+    # k = 7
+    # # Читаємо CSV-файл
+    # capitals = pd.read_csv('capitals-location.csv')
+    # name_column = capitals.columns[0]  # Перша колонка містить назву країни
+    # coordinate_column = capitals.columns[2:4]  # Остання колонка містить числові дані
+#
+    # grouped_with_country_s = make_capital_dictionary(name_column, coordinate_column)
+    # print("\n --- --- --- --- --- --- --- --- --- --- ---\n")
+    # print(f"Countries quantity(capitals): {len(grouped_with_country_s)}")
+#
+    # clusters_f_gpu, centroids_f_gpu = k_means(grouped_with_country_s, k, 'GPU')
+#
+    # print("Capitals clusters (GPU):")
+    # show_clusters(clusters_f_gpu)
+#
+    # # Вивід даних для теплової карти
+#
+    # # Масив даних для теплової карти
+    # map_data = arr_for_map(grouped_with_country, grouped_with_country_s)
+    # print()
+    # print("--- --- --- --- --- Data for map --- --- --- --- ---")
+    # print(f"Countries quantity(two indicators and capitals): {len(map_data)}")
+    # print(map_data)
 
 
 
@@ -182,7 +242,7 @@ if __name__ == "__main__":
 
     X_e_gpu, labels_e_gpu = convert_clusters_to_arrays(clusters_e_gpu)
     output_scores(X_e_gpu, labels_e_gpu)
-    
+
 
 # --- --- -- Indicators --- --- ---
     # df = pd.read_csv('Worldbank-data-all.csv')
